d56389462ff1ca64fa0e5b2ec8d1b3ce6da8e168
alialfayed <alialfayed1@gmail.com>
2019-12-07 04:52:39 +0200

    apk

--- app/src/main/java/com/alialfayed/utils/ServiceOnBackground.kt
+++ app/src/main/java/com/alialfayed/utils/ServiceOnBackground.kt
@@ -23,7 +23,7 @@ import kotlinx.coroutines.NonCancellable.start
  * Class do :
  * Created by ( Eng Ali)
  */
-class ServiceOnBackground(): Service() {
+class ServiceOnBackground: Service() {
 
     private  var mdatabaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("Database")
 
@@ -52,9 +52,7 @@ class ServiceOnBackground(): Service() {
                 }
             }
         })
-
         return START_NOT_STICKY
-
 
     }
 
